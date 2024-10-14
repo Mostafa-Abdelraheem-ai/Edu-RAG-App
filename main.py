683d9c9fe4f0e0433ea3599bb4d0b0d66e8cc818
mostafa abdelrahhem <mostafaabdelrahhem56@gmail.com>
2024-10-14 18:37:15 +0300

    reformatting the structure!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
+
 app = FastAPI()
 
-@app.get("/welcome")
-def welcom():
-    return {"message": "Heloo world!"}
+app.include_router(base.base_router)
